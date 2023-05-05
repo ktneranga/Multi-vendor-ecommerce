f8d8650ae3025112fc7f395f26dad8b3a3776d6c
Teran Neranga <terann@orelit.com>
2023-05-05 13:10:35 +0530

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { LoginPage } from './Routes'
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<LoginPage/>}/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
